42ba35791436eb18a39c6d345e0a7da43ffa6124
Angel Cruz <angelfortinoc@gmail.com>
2023-07-25 22:35:05 -0600

    LoadingScreen

--- Assets/Scripts/WEB Scripts/ChangeScene.cs
+++ Assets/Scripts/WEB Scripts/ChangeScene.cs
@@ -1,20 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class ChangeScene : MonoBehaviour
 {
+    public GameObject panel;
+    //public Text text1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        panel.SetActive(false);
     }
 
     // Update is called once per frame
         private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Loading Screen"){
+        if(other.tag == "Reticle"){
+        // Add scene info to the same scene SceneManager.LoadScene("Planta1", LoadSceneMode.Additive);
+        SceneManager.LoadScene("Loading screen");
+        panel.SetActive(true);
         print("hello");
+        }
+    }
+    private void OnTriggerExit(Collider others)
+    {
+        if(others.tag == "Reticle"){
+        panel.SetActive(false);
         }
     }
 }
